da6fdb09b7a0ced49d6a7b9994e5a3e78e695627
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-12-13 17:23:26 +0100

    Add unit Point2D.

--- src/units/mod.rs
+++ src/units/mod.rs
@@ -1,4 +1,7 @@
 //! Standard units used across Luos drivers
 
 mod angle;
+mod point;
+
 pub use self::angle::Angle;
+pub use self::point::Point2D;
---
+++ src/units/point.rs
@@ -0,0 +1,46 @@
+/// Represent a 2D point (x, y)
+///
+/// # Examples
+///
+/// ```
+/// use luos::units::Point2D;
+///
+/// let pt = Point2D::origin();
+///
+/// assert_eq!(pt.x, 0.0);
+/// assert_eq!(pt.y, 0.0);
+/// ```
+#[derive(Clone, Copy, Debug)]
+pub struct Point2D {
+    pub x: f32,
+    pub y: f32,
+}
+impl Point2D {
+    pub fn new(x: f32, y: f32) -> Point2D {
+        Point2D { x, y }
+    }
+    pub fn origin() -> Point2D {
+        Point2D::new(0.0, 0.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    extern crate rand;
+    use self::rand::Rng;
+
+
+    #[test]
+    fn create_point() {
+        let mut rng = rand::thread_rng();
+
+        let x: f32 = rng.gen();
+        let y: f32 = rng.gen();
+        let pt = Point2D::new(x, y);
+
+        assert_eq!(pt.x, x);
+        assert_eq!(pt.y, y);
+    }
+}
