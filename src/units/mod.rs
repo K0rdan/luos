764d926912fb8d21e27bb8de40263c6a975d85d7
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2017-12-14 12:38:02 +0100

    add frequency unit

--- src/units/mod.rs
+++ src/units/mod.rs
@@ -3,7 +3,9 @@
 mod angle;
 mod point;
 mod rot_speed;
+mod frequency;
 
 pub use self::angle::Angle;
 pub use self::point::Point2D;
 pub use self::rot_speed::RotSpeed;
+pub use self::frequency::Frequency;
